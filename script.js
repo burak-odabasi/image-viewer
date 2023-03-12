fce57d17c3b5b7c5b071d58bb0706782f050a146
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-12 23:09:17 +0300

    added images

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ const next = document.querySelector(".next");
 
 let image = document.createElement("img");
 
-const images = document.querySelectorAll(".animal");
+const images = document.querySelectorAll(".gallery img");
 images.forEach((img) => {
   img.addEventListener("click", (e) => {
     viewer.id = "active";
-    let animal = e.target.id;
     image.src = `./images/${img.id}-big.jpg`;
-    back.src = `./images/arrowback.png`;
-    next.src = `./images/arrownext.png`;
     image.style.height = "80vh";
-    image.style.Maxwidth = "60vw";
     image.style.border = "15px solid black";
     viewer.appendChild(image);
   });
