840c218238314eb3d4196825f7f341f132213ce5
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-13 23:20:27 +0300

    selecting an image will track its index

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const selectedImage = document.querySelector("#selected-image");
 const back = document.querySelector(".back");
 const next = document.querySelector(".next");
 const exit = document.querySelector(".exit");
+let currentImageIndex = 0;
 
 //when any thumbnail clicked, open viewer, show big image
 gallery.addEventListener("click", (e) => {
@@ -25,6 +26,7 @@ gallery.addEventListener("click", (e) => {
     selectedImage.src = e.target.src.replace("small", "big");
     viewer.id = "active";
     viewer.appendChild(selectedImage);
+    currentImageIndex = [...thumbnailImages].indexOf(e.target);
   }
 });
 
